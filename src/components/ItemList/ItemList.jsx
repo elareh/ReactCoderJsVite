93fb6a7f6253ca97ce563a9ad94714445539db7a
elareh <66442150+elareh@users.noreply.github.com>
2023-08-29 17:19:12 -0300

    2daPreEntrega+Curuchaga

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const ItemList = ({ items }) => {
     return (
         <div className="container">
-        <h1 className="text-center my-4">ItemList</h1>
+        <h1 className="text-center my-4">Nuestros Productos</h1>
         <div className="row">
             {items.map((item) => (
                 <div key={item.id} className="col-md-4 mb-4">
                     <div className="card">
                         <Link to={`/item/${item.id}`}>
                             <div className="card-body">
-                                <img src={item.image} alt={item.name} />
+                                <img src={item.image} alt={item.name} className="card-img-top img-fluid" />
                                 <h3 className="card-title">{item.name}</h3>
                                 <p className="card-text">{item.price}</p>
                                 <p className="card-text">{item.category}</p>
